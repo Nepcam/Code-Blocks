3a9bda2dd07494e3793fb3bc9629747afd5d8a5a
Cameron Nepe <cameron.nepe@gmail.com>
2020-09-11 09:39:05 +0000

    added block to main

--- main.cs
+++ main.cs
@@ -4,6 +4,19 @@ class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
   }
-
+  // variable to set up concatenation to write
+  string line = "";
+  // get StreamWriter and write to file inside the brackets
+  StreamWriter sw = new StreamWriter("test.csv");
+  // loop through the list 
+  foreach (Song song in playlist.SongList)
+  {
+    // line variable that has been concatenated
+    line = song.id.ToString() + "," + song.path + "," + song.rating.ToString() + "," + song.tags.ToString();
+    // write to file whats in the line variable
+    sw.WriteLine(line);
+  }
+  // close StreamWriter
+  sw.Close();
   
 }
