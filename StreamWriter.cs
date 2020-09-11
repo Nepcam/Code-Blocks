458cb81667614e0aaafe1f8fdcb7cc25501b1705
Cameron Nepe <cameron.nepe@gmail.com>
2020-09-11 09:35:33 +0000

    added comments

--- StreamWriter.cs
+++ StreamWriter.cs
@@ -1,12 +1,14 @@
+// variable to set up concatenation to write
 string line = "";
-
+// get StreamWriter and write to file inside the brackets
 StreamWriter sw = new StreamWriter("test.csv");
-
+  // loop through the list 
   foreach (Song song in playlist.SongList)
   {
+    // line variable that has been concatenated
     line = song.id.ToString() + "," + song.path + "," + song.rating.ToString() + "," + song.tags.ToString();
-
+    // write to file whats in the line variable
     sw.WriteLine(line);
   }
-
+// close StreamWriter
 sw.Close();
