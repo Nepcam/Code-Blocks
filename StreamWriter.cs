4a379bd858e9f9f337f8628be6c079e143d7e9af
Cameron Nepe <cameron.nepe@gmail.com>
2020-09-13 08:26:38 +0000

    using SafeFileDialog

--- StreamWriter.cs
+++ StreamWriter.cs
@@ -12,3 +12,29 @@ StreamWriter sw = new StreamWriter("test.csv");
   }
 // close StreamWriter
 sw.Close();
+
+// using a saveFileDialog
+ SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+  string filter = "CSV file (*.csv)|*.csv| All Files (*.*)|*.*";
+  saveFileDialog1.Filter = filter;
+  string line = "";
+  StreamWriter sw = null;
+  try
+  {
+    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+    {
+      filter = saveFileDialog1.FileName;
+      sw = new StreamWriter(filter);
+      foreach (Song song in playlist.SongList)
+        {
+          line = song.id.ToString() + "," + song.path + "," + song.rating.ToString() + "," + song.tags.ToString();
+          sw.WriteLine(line);
+        }
+    }
+      sw.Close();
+      MessageBox.Show("File \"" + line + "\"");
+  }
+  catch (Exception ex)
+  {
+    throw new ApplicationException("This program didn't work", ex);
+  }
